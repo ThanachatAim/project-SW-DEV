05a734ce72275f062b70736b0514a3ca97247489
G10N(nick) <nick.supakorn@hotmail.com>
2023-03-31 00:40:14 +0700

    update update and delete reservation

--- nine/app/view/reservations/user/page.js
+++ nine/app/view/reservations/user/page.js
@@ -1,97 +1,232 @@
 "use client";
 import Navbar from "../../../../components/navbar";
+import Modal from "react-bootstrap/Modal";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
-import { useEffect } from "react";
-import { getReservations } from "@/logic/reservation";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { MdLocationCity } from "react-icons/md";
+import { useEffect, useState } from "react";
+import {
+  deleteReservation,
+  getReservations,
+  updateReservation,
+} from "@/logic/reservation";
 
 export default function page() {
+  const [data, setData] = useState(null);
+  const [show, setShow] = useState(false);
+  const [update, setUpdate] = useState(false);
+
   useEffect(() => {
-    getReservations().then((result) => {
-      console.log(result);
-    });
+    setUpdate(true);
   }, []);
+
+  useEffect(() => {
+    if (update) {
+      getReservations().then((result) => {
+        if (result.success) {
+          setShow(false);
+          if (result.data.length > 0) setShow(true);
+          setData(result.data);
+          setUpdate(false);
+        } else {
+          setShow(false);
+          alert("please login");
+          window.location.href = "/login";
+        }
+      });
+    }
+  }, [update]);
+
+  const deleteHandler = (id) => {
+    deleteReservation(id)
+      .then((result) => {
+        if (result.success) {
+          setUpdate(true);
+        } else {
+          alert("fail to delete");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // modal variable
+  const [active, setActive] = useState(false);
+  const [date, setDate] = useState("");
+  const [reservationId, setReservationId] = useState(null);
+  const handleCloseModal = () => {
+    if (date !== "") {
+      updateReservation(reservationId, date)
+        .then((result) => {
+          if (result.success) {
+            setUpdate(true);
+          } else {
+            alert("fail to update");
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+    setDate("");
+    setActive(false);
+  };
+
+  const handleShowModal = (value) => {
+    setActive(true);
+    setReservationId(value);
+    console.log(value);
+  };
+
+  const updateHandler = (id) => {
+    handleShowModal(id);
+  };
+
+  const svgColor = (i) => {
+    if (i % 3 === 1) {
+      return (
+        <svg
+          className="bd-placeholder-img flex-shrink-0 me-2 rounded my-auto"
+          width="32"
+          height="32"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          aria-label="Placeholder: 32x32"
+          preserveAspectRatio="xMidYMid slice"
+          focusable="false"
+        >
+          <title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#007bff"></rect>
+          <text x="50%" y="50%" fill="#007bff" dy=".3em">
+            32x32
+          </text>
+        </svg>
+      );
+    } else if (i % 3 === 2) {
+      return (
+        <svg
+          className="bd-placeholder-img flex-shrink-0 me-2 rounded my-auto"
+          width="32"
+          height="32"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          aria-label="Placeholder: 32x32"
+          preserveAspectRatio="xMidYMid slice"
+          focusable="false"
+        >
+          <title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#e83e8c"></rect>
+          <text x="50%" y="50%" fill="#e83e8c" dy=".3em">
+            32x32
+          </text>
+        </svg>
+      );
+    } else {
+      return (
+        <svg
+          className="bd-placeholder-img flex-shrink-0 me-2 rounded my-auto"
+          width="32"
+          height="32"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          aria-label="Placeholder: 32x32"
+          preserveAspectRatio="xMidYMid slice"
+          focusable="false"
+        >
+          <title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#6f42c1"></rect>
+          <text x="50%" y="50%" fill="#6f42c1" dy=".3em">
+            32x32
+          </text>
+        </svg>
+      );
+    }
+  };
+
   return (
     <>
       <Navbar />
       <div className="container">
         <div className="my-3 p-3 bg-body rounded shadow-sm">
           <h6 className="border-bottom pb-2 mb-0">Your Reservation</h6>
-          <div className="d-flex text-muted pt-3">
-            <svg
-              className="bd-placeholder-img flex-shrink-0 me-2 rounded"
-              width="32"
-              height="32"
-              xmlns="http://www.w3.org/2000/svg"
-              role="img"
-              aria-label="Placeholder: 32x32"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-            >
-              <title>Placeholder</title>
-              <rect width="100%" height="100%" fill="#007bff"></rect>
-              <text x="50%" y="50%" fill="#007bff" dy=".3em">
-                32x32
-              </text>
-            </svg>
-
-            <p className="pb-3 mb-0 small lh-sm border-bottom">
-              <strong className="d-block text-gray-dark">@username</strong>
-              Some representative placeholder content, with some information
-              about this user. Imagine this being some sort of status update,
-              perhaps?
-            </p>
-          </div>
-          <div className="d-flex text-muted pt-3">
-            <svg
-              className="bd-placeholder-img flex-shrink-0 me-2 rounded"
-              width="32"
-              height="32"
-              xmlns="http://www.w3.org/2000/svg"
-              role="img"
-              aria-label="Placeholder: 32x32"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-            >
-              <title>Placeholder</title>
-              <rect width="100%" height="100%" fill="#e83e8c"></rect>
-              <text x="50%" y="50%" fill="#e83e8c" dy=".3em">
-                32x32
-              </text>
-            </svg>
-
-            <p className="pb-3 mb-0 small lh-sm border-bottom">
-              <strong className="d-block text-gray-dark">@username</strong>
-              Some more representative placeholder content, related to this
-              other user. Another status update, perhaps.
-            </p>
-          </div>
-          <div className="d-flex text-muted pt-3">
-            <svg
-              className="bd-placeholder-img flex-shrink-0 me-2 rounded"
-              width="32"
-              height="32"
-              xmlns="http://www.w3.org/2000/svg"
-              role="img"
-              aria-label="Placeholder: 32x32"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-            >
-              <title>Placeholder</title>
-              <rect width="100%" height="100%" fill="#6f42c1"></rect>
-              <text x="50%" y="50%" fill="#6f42c1" dy=".3em">
-                32x32
-              </text>
-            </svg>
-
-            <p className="pb-3 mb-0 small lh-sm border-bottom">
-              <strong className="d-block text-gray-dark">@username</strong>
-              This user also gets some representative placeholder content. Maybe
-              they did something interesting, and you really want to highlight
-              this in the recent updates.
-            </p>
-          </div>
+          {show ? (
+            data.map((reservation, i) => {
+              const value = reservation.reserveDate;
+              const date = new Date(value);
+              const res_date = date.getDate();
+              const res_month = date.getMonth() + 1;
+              const res_year = date.getFullYear();
+              const restaurant = reservation.restaurant;
+              return (
+                <div key={`res#${i}`} className="d-flex text-muted pt-3">
+                  {svgColor(i)}
+                  <p className="pb-2 mb-0 small lh-sm border-bottom ">
+                    <strong className="d-block text-gray-dark mb-1">
+                      {restaurant.name} @ {res_date}/{res_month}/{res_year}
+                    </strong>
+                    <MdLocationCity className="ms-2" size={18} /> :{" "}
+                    {restaurant.province}{" "}
+                    <BsFillTelephoneFill className="ms-2" /> : {restaurant.tel}
+                  </p>
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary ms-auto"
+                    onClick={() => updateHandler(reservation._id)}
+                  >
+                    <AiFillEdit />
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-outline-danger ms-1"
+                    onClick={() => deleteHandler(reservation._id)}
+                  >
+                    <AiFillDelete />
+                  </button>
+                </div>
+              );
+            })
+          ) : (
+            <>
+              <div>
+                <p className="pb-3 my-3 small lh-sm border-bottom ">
+                  <strong className="d-block text-gray-dark text-center">
+                    No Reservation
+                  </strong>
+                </p>
+              </div>
+            </>
+          )}
         </div>
       </div>
+
+      <Modal show={active} onHide={handleCloseModal}>
+        <Modal.Header closeButton id="head">
+          <Modal.Title>Reservation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div
+            className="from_box"
+            key="eventForm"
+            style={{ flex: 1, paddingLeft: "10px" }}
+          >
+            <div className="form-group">
+              <label className="my-1">Reservation Date</label>
+              <input
+                type="date"
+                className="form-control"
+                id="Inputdate"
+                onChange={(event) => setDate(event.target.value)}
+                value={date}
+              />
+            </div>
+            <button
+              className="btn btn-dark my-2"
+              onClick={() => {
+                handleCloseModal();
+              }}
+            >
+              submit
+            </button>
+          </div>
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
